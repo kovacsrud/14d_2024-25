f1d0c83d3c36b8311dcae7429b7ad0ca7947ab0a
kovacsrud <42613777+kovacsrud@users.noreply.github.com>
2024-01-25 08:58:08 +0100

    Konzol alapok

---
+++ Valtozok/Valtozok/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Valtozok
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("C# alapok");
+        }
+    }
+}
