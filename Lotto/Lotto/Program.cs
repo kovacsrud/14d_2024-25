4bdc3a21d7e305a0eb989a31ac4db4ed7b834580
kovacsrud <42613777+kovacsrud@users.noreply.github.com>
2024-01-30 13:34:08 +0100

    Lottó program

---
+++ Lotto/Lotto/Program.cs
@@ -0,0 +1,88 @@
+﻿namespace Lotto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            Console.Write("Hány számot húzunk?");
+            int hanySzam=Convert.ToInt32(Console.ReadLine());
+            Console.Write("Hány számból sorsolunk?");
+            int osszSzam=Convert.ToInt32(Console.ReadLine());
+
+            int[] tippek=new int[hanySzam];
+            int[] nyeroszamok=new int[hanySzam];
+
+            //Tippek bekérése
+            // 1 és osszSzam kozott kell lennie
+            // nem szerepelhet a tippek között
+            for(int i=0;i<hanySzam;i++)
+            {
+                Console.Write($"{i+1}.tipp:");
+                int temp=Convert.ToInt32(Console.ReadLine());
+
+                while(temp<1 || temp>osszSzam || tippek.Contains(temp))
+                {
+                    Console.Write("Hibás tipp, újra:");
+                    temp=Convert.ToInt32(Console.ReadLine());
+                }
+                tippek[i]=temp;
+
+            };
+
+            Array.Sort(tippek);
+
+            foreach(int i in tippek)
+            {
+                Console.Write($"{i} ");
+            };
+            Console.WriteLine();
+
+            //Sorsolás
+            for(int i=0;i< hanySzam;i++)
+            {
+                int temp=rand.Next(1,osszSzam+1);
+                while(nyeroszamok.Contains(temp))
+                {
+                    temp = rand.Next(1, osszSzam + 1);
+                };
+                nyeroszamok[i]=temp;
+            };
+
+            Array.Sort(nyeroszamok);
+
+            foreach (int i in nyeroszamok)
+            {
+                Console.Write($"{i} ");
+            };
+            //Találatok számának meghatározása
+            int talalat = 0;
+
+            for (int i = 0; i < tippek.Length; i++)
+            {
+                for (int j = 0; j < nyeroszamok.Length; j++)
+                {
+                    if (tippek[i] == nyeroszamok[j])
+                    {
+                        talalat++;
+                    }
+                }
+            }
+
+            Console.WriteLine($"Találatok száma:{talalat}");
+
+            talalat = 0;
+
+            for (int i = 0; i < tippek.Length; i++)
+            {
+                if (tippek.Contains(nyeroszamok[i]))
+                {
+                    talalat++;
+                }
+            }
+
+            Console.WriteLine($"Találatok száma:{talalat}");
+
+        }
+    }
+}
---
+++ Tomb_feladatok/Tomb_feladatok/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace Tomb_feladatok
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] szamok = new int[100];
+            Random rand= new Random();
+
+            for(int i = 0; i < szamok.Length; i++)
+            {
+                szamok[i] = rand.Next(-200,200+1);
+            }
+
+            int minPozitiv = int.MaxValue;
+            for (int i = 0; i < szamok.Length; i++)
+            {
+                if (szamok[i]>0 && szamok[i]<minPozitiv)
+                {
+                    minPozitiv = szamok[i];
+                }
+            }
+
+            Console.WriteLine($"Legkisebb nem nulla pozitív:{minPozitiv}");
+
+            int dbKettovelOszthato = 0;
+            int maxKettovelOszthato = int.MinValue;
+
+            for (int i = 0;i < szamok.Length; i++)
+            {
+                if (szamok[i]%2==0)
+                {
+                    dbKettovelOszthato++;
+                }
+                if (szamok[i]%2==0 && szamok[i]>maxKettovelOszthato)
+                {
+                    maxKettovelOszthato = szamok[i];
+                }
+            }
+
+            Console.WriteLine($"Darabszám:{dbKettovelOszthato},Legnagyobb 2-vel osztható:{maxKettovelOszthato}");
+
+
+        }
+    }
+}
