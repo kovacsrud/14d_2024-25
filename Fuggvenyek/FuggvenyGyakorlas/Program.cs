473323d6959063cf5088f555e019ee0e28562ba5
kovacsrud <42613777+kovacsrud@users.noreply.github.com>
2024-02-08 14:03:08 +0100

    Függvények

---
+++ Fuggvenyek/FuggvenyGyakorlas/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace FuggvenyGyakorlas
+{
+    internal class Program
+    {
+
+        public static int BetuSzamol(string szoveg,char betu)
+        {
+            int db = 0;
+
+            for (int i = 0; i < szoveg.Length; i++)
+            {
+                if (szoveg[i] == betu || szoveg[i] == betu)
+                {
+                    db++;
+                }
+            }
+
+
+            return db;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            //Írjon függvényt, amely a kapott string-ben megszámolja az 'a' betűk számát (írásmódtól függetlenül) és visszatérési értékként visszaadja azt
+
+            Console.WriteLine(BetuSzamol("valami,bármi akármi",'a'));
+            Console.WriteLine(BetuSzamol("valami,bármi akármi", 'm'));
+
+        }
+    }
+}
---
+++ Fuggvenyek/Fuggvenyek/Program.cs
@@ -0,0 +1,91 @@
+﻿using System.ComponentModel;
+
+namespace Fuggvenyek
+{
+    internal class Program
+    {
+
+        //Függvény overloading=> több függvényt írunk ugyanazzal a névvel, de eltérő paraméterlistával
+        public static void Kiir()
+        {
+            //a szoveg lokális változó
+            string szoveg = "Valami";
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, World!");
+        }
+
+        //public static void Kiir(string szoveg)
+        //{
+        //    Console.WriteLine(szoveg);
+        //}
+
+        /// <summary>
+        /// A függvény a szoveg-nél megadott szoveget írja ki annyiszor amennyi a hanyszor értéke
+        /// </summary>
+        /// <param name="szoveg">A kíírandó szöveg</param>
+        /// <param name="hanyszor">Hányszor kell kiírni a kiírandó szöveget</param>
+        public static void Kiir(string szoveg,int hanyszor=3)
+        {
+            for (int i = 0; i < hanyszor; i++)
+            {
+                Console.WriteLine(szoveg);
+            }
+        }
+
+        //Visszatérési értékkel rendelkező függvény
+        public static int Osszead(int a,int b)
+        {
+            return a + b;
+        }
+
+        public static double Osszead(double a,double b)
+        {
+            return a + b;
+        }
+
+        static void Main(string[] args)
+        {
+            //Amit itt deklarlálunk, az globális változó, az egész programban elérhető
+            Kiir();
+            Kiir("Paraméter", 2);
+            Kiir("Valami", 2);
+            Kiir("Háromszor");
+            Console.WriteLine(Osszead(11.3345, 22.778));
+            var osszeg = Osszead(134.789, 458.11267);
+            var masikOsszeg = Osszead(201, 688);
+
+            Console.WriteLine(osszeg);
+            Console.WriteLine(masikOsszeg);
+
+            string[] szovegek = { "Egy", "valami", "Zénó", "Opel", "Kórház utca" };
+            Tomblista(szovegek);
+            
+            string[] masszovegek = { "Kettő", "Bármi", "Ubul", "Fiat", "autópálya" };
+                        
+            Tomblista(masszovegek);
+
+            int[] szamok = { 33, 44, 89, 3345, 67, 33, 6674, 22 };
+
+            Tomblista(szamok);
+
+            bool[] logikai= { true, false, true, false, false };
+
+            Tomblista(logikai);
+
+        }
+
+        //Generikus típus a paraméter listában
+        private static void Tomblista<T>(T[] tomb)
+        {
+            for (int i = 0; i < tomb.Length; i++)
+            {
+                Console.Write(tomb[i] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
