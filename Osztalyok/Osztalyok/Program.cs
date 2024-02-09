dd2819b5d59c6705953930559541e62338ab692b
kovacsrud <42613777+kovacsrud@users.noreply.github.com>
2024-02-09 13:51:52 +0100

    Osztályok

---
+++ Osztalyok/Osztalyok/Program.cs
@@ -0,0 +1,56 @@
+﻿namespace Osztalyok
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Osztály: adatok és metódusok(függvények) összessége. Egy önálló egység a programban, amelyből
+            //több példányt is készíthetünk
+
+            //Szemely osztály példányosítása
+            Szemely ubul = new Szemely("Nagy","Ubul",1991,"Bélmegyer");
+            Szemely elek=new Szemely();
+
+            Szemely anna = new Szemely {
+                Vezeteknev="Kovács",
+                Keresztnev="Anna",
+                Lakhely="Miskolc"
+            };
+
+            //ubul.vezeteknev = "Kiss";
+            //ubul.keresztnev = "Ubul";
+            //ubul.szuletesiEv = 1998;
+            //ubul.lakhely = "Kétegyháza";
+
+            Szemely agnes=new Szemely("Kis","Ágnes",2001,"Keszthely");
+            //agnes.vezeteknev = "Kósa";
+            //agnes.keresztnev = "Ágnes";
+            //agnes.lakhely = "Orosháza";
+            //agnes.szuletesiEv = 1986;
+
+            //Console.WriteLine(agnes.keresztnev);
+            //Console.WriteLine(agnes.vezeteknev);
+            //Console.WriteLine(ubul.vezeteknev);
+            //Console.WriteLine(ubul.keresztnev);
+            
+
+            Console.WriteLine($"{agnes.Vezeteknev} {agnes.Keresztnev}");
+
+
+            //polimorfizmus: ugyanaz a metódushívás eltérő eredményt eredményez két különböző példánynál
+            Console.WriteLine(ubul.Eletkor());
+            agnes.SetSzuletesiEv(1988);
+            Console.WriteLine(agnes.Eletkor());
+            Console.WriteLine(agnes.GetSzuletesiEv());
+
+            //Készítsen osztályt Auto néven! 
+            //A következő property-k legyenek az osztályban:
+            //Marka,Tipus,GyartasiEv,Szin,FutottKM
+            //Csak paraméteres konstruktorral lehessen példányosítani!
+            //A FutottKm ne lehessen negatív
+            //A GyartasiEv ne lehessen kisebb mint 1990, ne lehessen nagyobb mint az aktualis év
+
+
+        }
+    }
+}
---
+++ Osztalyok/Osztalyok/Szemely.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Osztalyok
+{
+    public class Szemely
+    {
+        //adatmezők, adattagok
+        //az osztály adatmezőit csak metódusokon keresztül lehessen elérni
+        //az osztály csak azokat az adatokat/metódusokat tegye láthatóvá, amelyek a megfeleő
+        //működéshez okvetlenül szükségesek
+
+        //Property: olyan változó, amely lekérdező és beállító függvényt is tartalmaz
+
+       private string vezeteknev;
+       public string Vezeteknev { 
+            get { return vezeteknev; } 
+            set {
+                if (value.Length > 1)
+                {
+                    vezeteknev = value;
+                } else
+                {
+                    throw new ArgumentException("Hibás vezetéknév");
+                }
+
+            }
+        }
+       public string Keresztnev { get; set; }
+
+        private int szuletesiEv;
+
+       public string Lakhely { get; set; }
+
+
+        public Szemely(string vezeteknev,string keresztnev,int szulev,string lakhely)
+        {
+            Vezeteknev= vezeteknev;
+            Keresztnev= keresztnev;
+            szuletesiEv=szulev;
+            Lakhely= lakhely;
+        }
+
+        public Szemely()
+        {
+            
+        }
+
+
+
+
+        //metódusok
+        public int Eletkor()
+        {
+            return DateTime.Now.Year-szuletesiEv;
+        }
+
+        public int GetSzuletesiEv()
+        {
+            return szuletesiEv;
+        }
+
+        public void SetSzuletesiEv(int szuletesiEv)
+        {
+            if (szuletesiEv>1900 && szuletesiEv<=DateTime.Now.Year)
+            {
+                this.szuletesiEv = szuletesiEv;
+            } else
+            {
+                this.szuletesiEv=DateTime.Now.Year;
+            }
+        }
+
+    }
+}
