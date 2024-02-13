a2e3da6ca9b07bdcbf238fd379b61c62991f7ce7
kovacsrud <42613777+kovacsrud@users.noreply.github.com>
2024-02-13 13:42:40 +0100

    Lista funkciók gyakorlása

---
+++ Osztalyok/Osztalyok/Auto.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Osztalyok
+{
+    public class Auto
+    {
+       
+
+        public string Marka { get; set; }
+        public string Tipus { get; set; }
+
+        private int gyartasiev;
+        public int GyartasiEv { 
+            get 
+            {
+                return gyartasiev;
+            }
+            set
+            {
+                if(value< 1990 || value > DateTime.Now.Year)
+                {
+                    throw new ArgumentException("Hibás gyártási év!");
+                } else
+                {
+                    gyartasiev = value;
+                }
+            } 
+        }
+        public string Szin { get; set; }
+
+        private int futottkm;
+        public int FutottKM {
+            get
+            {  return futottkm; }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("A Futott km nem lehet kisebb mint 0!");
+                } else
+                {
+                    futottkm = value;
+                }
+            } 
+        
+        }
+
+        public Auto(string marka, string tipus, int gyartasiEv, string szin, int futottKM)
+        {
+            Marka = marka;
+            Tipus = tipus;
+            GyartasiEv = gyartasiEv;
+            Szin = szin;
+            FutottKM = futottKM;
+        }
+
+        public Auto()
+        {
+                
+        }
+
+
+    }
+}
--- Osztalyok/Osztalyok/Program.cs
+++ Osztalyok/Osztalyok/Program.cs
@@ -50,6 +50,87 @@
             //A FutottKm ne lehessen negatív
             //A GyartasiEv ne lehessen kisebb mint 1990, ne lehessen nagyobb mint az aktualis év
 
+            //Autó lista létrehozása
+            Random random = new Random();
+            List<string> markak = new List<string>() {"Opel","Ford","BMW","Fiat","Citroen","Peugeot","VolksWagen","Mazda","Skoda"};
+            List<string> tipusok = new List<string>() {"Focus","Mondeo","Fabia","Xsara","Bravo","407","320d","Premacy" };
+            List<string> szinek = new List<string>() { "piros","kék","zöld","sárga","fekete","fehér","szürke"};
+
+            List<Auto> autok=new List<Auto>();
+
+            for(int i = 0; i < 1000; i++)
+            {
+                var markaIndex = random.Next(0, markak.Count);
+                var tipusIndex = random.Next(0, tipusok.Count);
+                var szinIndex = random.Next(0, szinek.Count);
+                var futottKm = random.Next(100, 300000+1);
+                var gyartasiev=random.Next(1990,DateTime.Now.Year+1);
+
+                autok.Add(new Auto
+                {
+                    Marka = markak[markaIndex],
+                    Tipus = tipusok[tipusIndex],
+                    Szin = szinek[szinIndex],
+                    FutottKM = futottKm,
+                    GyartasiEv = gyartasiev
+                });
+
+            }
+
+            Console.WriteLine($"Autók száma:{autok.Count}");
+
+            var fordFocusok = autok.FindAll(x=>x.Marka=="Ford" && x.Tipus=="Focus");
+
+            Console.WriteLine($"Focusok száma:{fordFocusok.Count}");
+
+            foreach(var ford in fordFocusok)
+            {
+                Console.WriteLine($"{ford.Szin},{ford.FutottKM},{ford.GyartasiEv}");
+            }
+
+            Console.WriteLine("----------------------------------");
+            var fordMondeok = autok.Where(x => x.Marka == "Ford" && x.Tipus == "Mondeo");
+
+
+            //foreach (var ford in fordMondeok)
+            //{
+            //    Console.WriteLine($"{ford.Szin},{ford.FutottKM},{ford.GyartasiEv}");
+            //}
+
+            if (autok.Any(x=>x.Marka.ToLower()=="BMW".ToLower() && x.Tipus.ToLower() =="Fabia".ToLower() ))
+            {
+                Console.WriteLine("Van ilyen!");
+            } else
+            {
+                Console.WriteLine("Nincs ilyen!");
+            }
+
+            var atlagFutott = autok.Average(x=>x.FutottKM);
+            var minFutott = autok.Min(x => x.FutottKM);
+            var maxFutott = autok.Max(x => x.FutottKM);
+
+            Console.WriteLine($"Átlagos futás:{atlagFutott},Min:{minFutott},Max:{maxFutott}");
+
+
+            //Hány darab van márkánként az autókból?
+            //Összesítési feladat
+
+            var stat = autok.ToLookup(x=>x.Marka);
+
+            foreach (var i in stat)
+            {
+                Console.WriteLine($"{i.Key},{i.Count()},{i.Average(x=>x.FutottKM)}");
+            }
+
+            //Hány darab van márkánként és típusonként?
+            var statTipus = autok.ToLookup(x => new {x.Marka,x.Tipus});
+
+            foreach (var i in statTipus)
+            {
+                Console.WriteLine($"{i.Key.Marka} {i.Key.Tipus},{i.Count()}");
+            }
+            
+            
 
         }
     }
