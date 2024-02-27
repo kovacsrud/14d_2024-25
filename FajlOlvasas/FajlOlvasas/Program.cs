63d3babfaa349fa85e95cd63b8ee2fbff19c93d5
kovacsrud <42613777+kovacsrud@users.noreply.github.com>
2024-02-27 13:58:05 +0100

    Fájl beolvasás

---
+++ FajlOlvasas/FajlOlvasas/Program.cs
@@ -0,0 +1,49 @@
+﻿using System.IO;
+using System.Reflection.PortableExecutable;
+using System.Text;
+
+namespace FajlOlvasas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Nem annyira szép, de működik
+            //StreamReader reader=null;
+			try
+			{
+                FileStream fajl = new FileStream("hegyek.txt", FileMode.Open);
+                //StreamReader reader=new StreamReader(fajl,Encoding.Default);
+
+
+                //Using blokk használata, a legjobb megoldás
+                using (StreamReader reader = new StreamReader(fajl, Encoding.Default))
+                {                   
+                    while (!reader.EndOfStream)
+                    {
+                        Console.WriteLine(reader.ReadLine());
+                    }
+                }
+
+                //reader.ReadLine();
+                //while (!reader.EndOfStream)
+                //{
+                //    Console.WriteLine(reader.ReadLine());
+                //}
+
+
+                //reader.Close();
+
+            }
+			catch (Exception ex)
+			{
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                //fájl bezárása
+                //reader.Close();
+            }
+        }
+    }
+}
