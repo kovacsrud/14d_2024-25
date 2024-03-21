95c139e3dc230f133bb064e2fb44989e23ec015a
kovacsrud <42613777+kovacsrud@users.noreply.github.com>
2024-03-21 12:00:41 +0100

    Kiralynok

---
+++ Kiralynok/Kiralynok/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Kiralynok
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Tabla tabla = new Tabla("*");
+            tabla.Megjelenit();
+            tabla.Elhelyez(8);
+            Console.WriteLine("-----------------");
+            tabla.Megjelenit();
+            //Console.WriteLine(tabla.UresOszlop(2));
+            //Console.WriteLine(tabla.UresOszlop(5));
+            //Console.WriteLine(tabla.UresSor(2));
+            //Console.WriteLine(tabla.UresSor(5));
+            Console.WriteLine($"Üres sorok:{tabla.UresSorokSzama},üres oszlopok:{tabla.UresOszlopokSzama}");
+            //Console.WriteLine(tabla.ToString());
+            tabla.FajlbaIr();
+
+
+        }
+    }
+}
---
+++ Kiralynok/Kiralynok/Tabla.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kiralynok
+{
+    public class Tabla
+    {
+        private string[,] T;
+        private string UresCella;
+
+        public int UresOszlopokSzama { get; set; }
+        public int UresSorokSzama { get; set; }
+
+        public Tabla(string urescella)
+        {
+            T = new string[8, 8];
+            UresCella = urescella;
+            for (int i = 0; i < T.GetLength(0); i++)
+            {
+                for (int j = 0; j < T.GetLength(1); j++)
+                {
+                    T[i, j] = UresCella;
+                }
+            }
+
+        }
+
+        public void Megjelenit()
+        {
+            for (int i = 0; i < T.GetLength(0); i++)
+            {
+                for (int j = 0; j < T.GetLength(1); j++)
+                {
+                    Console.Write(T[i,j]);
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public void Elhelyez(int elhelyezDb)
+        {
+            Random random = new Random();
+            for (int i = 0;i < elhelyezDb; i++)
+            {
+                var sor = random.Next(0, 8);
+                var oszlop=random.Next(0, 8);
+                var aktualisElem = T[sor, oszlop];
+                while (aktualisElem == "K")
+                {
+                    sor= random.Next(0, 8);
+                    oszlop=random.Next(0, 8);
+                    aktualisElem= T[sor, oszlop];
+                }
+                T[sor, oszlop] = "K";
+            }
+
+            UresSzamol();
+        }
+
+        public bool UresOszlop(int oszlop)
+        {
+            bool IsUresOszlop = true;
+            for (int i = 0; i < T.GetLength(1); i++)
+            {
+                if (T[i,oszlop]=="K")
+                {
+                    IsUresOszlop = false;
+                }
+            }
+
+            return IsUresOszlop;
+        }
+
+        public bool UresSor(int sor)
+        {
+            bool IsUresSor = true;
+            for(int i = 0; i < T.GetLength(0); i++)
+            {
+                if (T[sor, i] == "K")
+                {
+                    IsUresSor=false;
+                }
+            }
+
+            return IsUresSor;
+        }
+
+        public void UresSzamol()
+        {
+            for (int i = 0;i < T.GetLength(0); i++)
+            {
+                if (UresSor(i))
+                {
+                    UresSorokSzama += 1;
+                }
+            }
+            for(int i = 0;i<T.GetLength(1) ; i++)
+            {
+                if (UresOszlop(i))
+                {
+                    UresOszlopokSzama += 1;
+                }
+            }
+        }
+
+        public override string ToString()
+        {
+            string tabla = "";
+            for (int i = 0; i < T.GetLength(0); i++)
+            {
+                for (int j = 0; j < T.GetLength(1); j++)
+                {
+                    tabla += T[i, j];
+                }
+                tabla += System.Environment.NewLine;
+            }
+            tabla += "        " + System.Environment.NewLine;
+
+            return tabla;
+        }
+
+        public void FajlbaIr()
+        {
+            string tablaStr = "";
+            for(int i = 0;i<64 ; i++)
+            {
+                Tabla tabla = new Tabla("*");
+                tabla.Elhelyez(i + 1);
+                tablaStr += tabla.ToString();
+            }
+
+            try
+            {
+                File.WriteAllText("tablak64.txt", tablaStr);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+    }
+}
