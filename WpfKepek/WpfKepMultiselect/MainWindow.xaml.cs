d6d3f9b343de624e1f879f5b32bef984930b12a1
kovacsrud <42613777+kovacsrud@users.noreply.github.com>
2024-04-04 11:48:56 +0200

    Képek kezelése WPF-ben

---
+++ WpfKepek/WpfKepMultiselect/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Win32;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfKepMultiselect
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonBetolt_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog dialog=new OpenFileDialog();
+            dialog.Multiselect = true;
+            if (dialog.ShowDialog()==true)
+            {
+                if (dialog.FileNames.Length >= 4)
+                {
+                    kep1.Source=new BitmapImage(new Uri(dialog.FileNames[0]));
+                    kep2.Source = new BitmapImage(new Uri(dialog.FileNames[1]));
+                    kep3.Source = new BitmapImage(new Uri(dialog.FileNames[2]));
+                    kep4.Source = new BitmapImage(new Uri(dialog.FileNames[3]));
+                }
+            }
+        }
+    }
+}
---
+++ WpfKepek/WpfKepek/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Win32;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfKepek
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            imageMasodik.Source = new BitmapImage(new Uri(@"d:\testpics\fish.png"));
+        }
+
+        private void buttonOpen_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog dialog= new OpenFileDialog();
+                        
+            dialog.Filter = ".jpg|*.jpg|.png|*.png|összes fájl|*.*"; ;
+
+
+            if (dialog.ShowDialog()==true)
+            {
+               
+                imageHarmadik.Source = new BitmapImage(new Uri(dialog.FileName));
+                imageHarmadik.Stretch = Stretch.Uniform;
+            }
+        }
+
+
+    }
+}
