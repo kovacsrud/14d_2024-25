4f7cac1fae337636f3589c531207a3774971f8c6
kovacsrud <42613777+kovacsrud@users.noreply.github.com>
2024-05-16 10:19:11 +0200

    Jegyzettömb menü kialakítás

---
+++ WpfJegyzettomb/WpfJegyzettomb/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfJegyzettomb
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void menuitemNevjegy_Click(object sender, RoutedEventArgs e)
+        {
+            Nevjegy nevjegy = new Nevjegy();
+            nevjegy.ShowDialog();
+        }
+
+        private void menuitemKilepes_Click(object sender, RoutedEventArgs e)
+        {
+            System.Environment.Exit(0);
+        }
+
+        private void menuitemMegnyitas_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
