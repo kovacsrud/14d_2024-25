2ffd07f20f6b71705f77a759f44d93f3520e4fc3
kovacsrud <42613777+kovacsrud@users.noreply.github.com>
2024-06-04 13:27:11 +0200

    Hash készítő program

---
+++ WpfHash/WpfHash/Hash.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+using System.IO;
+
+namespace WpfHash
+{
+    public class Hash
+    {
+        public string Md5Hash(string szoveg)
+        {
+            MD5CryptoServiceProvider md5Hash = new MD5CryptoServiceProvider();
+
+            byte[] data=null;
+
+            if (File.Exists(szoveg))
+            {
+                //Fájl betöltés és hash
+                data= File.ReadAllBytes(szoveg);
+                
+            } else
+            {
+                //Szövegből hash
+                data = new UTF8Encoding().GetBytes(szoveg);
+            }
+
+            //byte[] 
+
+            return ByteToHash(md5Hash.ComputeHash(data));
+        }
+
+        private string ByteToHash(byte[] data)
+        {
+            StringBuilder hash = new StringBuilder();
+
+            for (int i = 0; i < data.Length; i++)
+            {
+                hash.Append(data[i].ToString("x2"));
+            }
+
+            return hash.ToString();
+        }
+
+    }
+}
---
+++ WpfHash/WpfHash/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.Win32;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfHash
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Hash hash=new Hash();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void textboxSzoveg_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            try
+            {
+                textboxHash.Text = hash.Md5Hash(textboxSzoveg.Text);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            
+        }
+
+        private void buttonTallozas_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog dialog= new OpenFileDialog();
+            try
+            {
+                if (dialog.ShowDialog()==true)
+                {
+                    textboxFileHash.Text = hash.Md5Hash(dialog.FileName);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
