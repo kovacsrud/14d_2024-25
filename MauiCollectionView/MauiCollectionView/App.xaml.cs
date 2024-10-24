682f8d2d7b3c4e7f0bb6ad2a72e8f71edef8f27d
kovacsrud <kovacs.rud@gmail.com>
2024-10-24 13:53:14 +0200

    Szűrés funkció

--- MauiCollectionView/MauiCollectionView/App.xaml.cs
+++ MauiCollectionView/MauiCollectionView/App.xaml.cs
@@ -6,7 +6,7 @@
         {
             InitializeComponent();
 
-            MainPage = new ListaPage();
+            MainPage = new AppShell();
         }
     }
 }
--- MauiCollectionView/MauiCollectionView/ListaPage.xaml.cs
+++ MauiCollectionView/MauiCollectionView/ListaPage.xaml.cs
@@ -30,4 +30,30 @@ public partial class ListaPage : ContentPage
             Hegyek.Add(new Hegy(reader.ReadLine()));
         }
     }
+
+    private async void buttonSzur_Clicked(object sender, EventArgs e)
+    {
+        try
+        {
+            var result = Hegyek.Where(x => x.Magassag >= Convert.ToInt32(entryMagassag.Text));
+
+            if (result.Count()>0)
+            {
+                collectionHegyek.ItemsSource=result;
+            } else
+            {
+                await DisplayAlert("Info","Nincs találat","Ok");
+            }
+            
+        }
+        catch (Exception ex)
+        {
+            await DisplayAlert("Hiba",ex.Message,"Ok");        
+        }
+    }
+
+    private void buttonVissza_Clicked(object sender, EventArgs e)
+    {
+        collectionHegyek.ItemsSource = Hegyek;
+    }
 }
