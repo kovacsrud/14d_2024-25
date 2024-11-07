9b4335cb333c293e65c3f55e443bd1c29fd8a03c
kovacsrud <kovacs.rud@gmail.com>
2024-11-07 09:57:46 +0100

    Egyedi ablak

---
+++ WpfMonthyHall14d/WpfMonthyHall14d/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfMonthyHall14d
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void minGomb_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            WindowState=WindowState.Minimized;
+        }
+
+        private void maxGomb_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (WindowState == WindowState.Maximized)
+            {
+                WindowState = WindowState.Normal;
+            } else
+            {
+                WindowState = WindowState.Maximized;
+            }
+        }
+
+        private void closeGomb_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            Close();
+        }
+
+        private void rectHeader_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+    }
+}
