72eeb037af2467109af082ae9c851df4cf7bd13d
kovacsrud <kovacs.rud@gmail.com>
2024-12-06 11:25:11 +0100

    Maui Hybrid App

---
+++ MauiHybridStart/MauiHybridStart/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MauiHybridStart
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MauiHybridStart/MauiHybridStart/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace MauiHybridStart
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
