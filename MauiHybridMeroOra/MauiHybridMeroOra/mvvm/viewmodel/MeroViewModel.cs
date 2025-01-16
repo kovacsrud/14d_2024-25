44d6c3ec6c7312461b3bc525d1fe105a7ac8bd97
kovacsrud <kovacs.rud@gmail.com>
2025-01-16 10:00:29 +0100

    Meroora módosítás, törlés

--- MauiHybridMeroOra/MauiHybridMeroOra/mvvm/viewmodel/MeroViewModel.cs
+++ MauiHybridMeroOra/MauiHybridMeroOra/mvvm/viewmodel/MeroViewModel.cs
@@ -12,15 +12,12 @@ namespace MauiHybridMeroOra.mvvm.viewmodel
     public class MeroViewModel
     {
         public List<MeroOra> Oraallasok {  get; set; }=new List<MeroOra>();
-        public MeroOra AktualisMeroOra { get; set; } = new MeroOra();
+        public MeroOra AktualisOraallas { get; set; } = new MeroOra();
+        public bool Modositas { get; set; } = false;
 
         public MeroViewModel()
         {
-            //var ujadat=new MeroOra { Termeles=10,Fogyasztas=15};
-            //UjOraAllas(ujadat);
-            //var megEgyAdat = new MeroOra { Termeles = 20, Fogyasztas = 25 };
-            //UjOraAllas(megEgyAdat);
-
+          
             AdatLetoltes();   
         }
 
@@ -34,6 +31,7 @@ namespace MauiHybridMeroOra.mvvm.viewmodel
             try
             {
                 App.MeroRepository.NewItem(meroora);
+                Application.Current.MainPage.DisplayAlert("Info", App.MeroRepository.StatusMsg, "Ok");
                 AdatLetoltes();
             }
             catch (Exception ex)
@@ -42,5 +40,42 @@ namespace MauiHybridMeroOra.mvvm.viewmodel
                 Application.Current.MainPage.DisplayAlert("Hiba", ex.Message, "Ok");
             }
         }
+
+        public void ModositOraallas(MeroOra meroora) {
+            try
+            {
+                App.MeroRepository.UpdateItem(meroora);
+                Application.Current.MainPage.DisplayAlert("Info", App.MeroRepository.StatusMsg, "Ok");
+                AdatLetoltes();
+            }
+            catch (Exception ex)
+            {
+                Application.Current.MainPage.DisplayAlert("Hiba", ex.Message, "Ok");
+            }
+        }
+
+        public async void TorolOraallas(MeroOra meroora)
+        {
+            try
+            {
+                var result = await Application.Current.MainPage.DisplayAlert("Törlés", "Biztosan törli?", "Igen", "Mégse");
+
+                if (result)
+                {
+                    App.MeroRepository.DeleteItem(meroora);
+                    await Application.Current.MainPage.DisplayAlert("Info", App.MeroRepository.StatusMsg, "Ok");
+                    AdatLetoltes();
+
+                }
+            }
+            catch (Exception ex)
+            {
+                Application.Current.MainPage.DisplayAlert("Hiba", ex.Message, "Ok");
+            }
+
+
+            
+        }
+
     }
 }
