ef7f9d7e7d90032e779603d8ed1a6b6256e0a324
kovacsrud <kovacs.rud@gmail.com>
2025-01-30 14:13:52 +0100

    Fájlkezelés binárisan

---
+++ BinarisFajlkezeles/BinarisFajlkezeles/Program.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Concurrent;
+using System.Text;
+
+namespace BinarisFajlkezeles
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var szovegFajl = File.ReadAllBytes("szinek.txt");
+
+            foreach (var bajt in szovegFajl)
+            {
+                //Console.Write(bajt+" ");
+                //Console.Write(Convert.ToString(bajt,2).PadLeft(8,'0')+" ");
+            }
+
+            //Console.WriteLine(Encoding.UTF8.GetString(szovegFajl));
+
+            var zipFajl = File.ReadAllBytes("MonthyHallGame.zip");
+
+            //Console.WriteLine(Encoding.UTF8.GetString(zipFajl));
+
+            using (MemoryStream ms=new MemoryStream(zipFajl))
+            {
+                using (BinaryReader br=new BinaryReader(ms))
+                {
+                    var signature=br.ReadBytes(4);
+                    //Console.WriteLine(BitConverter.ToInt32(signature));
+                    Console.WriteLine($"Signature:{BitConverter.ToString(signature)}");   
+                    var version=br.ReadBytes(2);
+                    //Console.WriteLine(BitConverter.ToString(version));
+                    Console.WriteLine($"Version:{BitConverter.ToInt16(version)}");
+                    var flags=br.ReadBytes(2);
+                    Console.WriteLine($"Flags:{BitConverter.ToString(version)}");
+                    var compression=br.ReadBytes(2);
+                    Console.WriteLine($"Compression:{BitConverter.ToString(compression)}");
+                    var modtime=br.ReadBytes(2);
+                    Console.WriteLine($"Modification time:{BitConverter.ToString(modtime)}");
+                    var moddate = br.ReadBytes(2);
+                    Console.WriteLine($"Modification date:{BitConverter.ToString(moddate)}");
+                    var crc=br.ReadBytes(4);
+                    Console.WriteLine($"CRC:{BitConverter.ToString(crc)}");
+                    var compressed=br.ReadBytes(4);
+                    Console.WriteLine($"Compressed:{BitConverter.ToString(compressed)}");
+                    var uncompressed = br.ReadBytes(4);
+                    Console.WriteLine($"Uncompressed:{BitConverter.ToString(uncompressed)}");
+                    var filenameLength = br.ReadBytes(2);
+                    Console.WriteLine($"File name length:{BitConverter.ToInt16(filenameLength)}");
+                    var extraLength = br.ReadBytes(2);
+                    Console.WriteLine($"Extra length:{BitConverter.ToInt16(extraLength)}");
+                    var filename=br.ReadBytes(BitConverter.ToInt16(filenameLength));
+                    Console.WriteLine($"Fajlnev:{Encoding.UTF8.GetString(filename)}");
+
+                }
+            }
+
+        }
+    }
+}
---
+++ BinarisFajlkezeles/Sajatformatum/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Text;
+
+namespace Sajatformatum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string fajlnev = "szinek.txt";
+            var szovegfajl=File.ReadAllBytes(fajlnev);
+            var fajlnevBin=Encoding.UTF8.GetBytes(fajlnev);
+
+            Console.WriteLine(szovegfajl.Length);
+            Console.WriteLine(fajlnevBin.Length);
+            //saját fileformátum:filenév hossza ?/filenév változó/fájl hossza ?/fájl adatok változó
+
+            var fajlnevHosszBin = BitConverter.GetBytes(fajlnevBin.Length);
+            var fajlhosszBin=BitConverter.GetBytes(szovegfajl.Length);
+
+            Console.WriteLine(fajlnevHosszBin.Length);
+            Console.WriteLine(fajlhosszBin.Length);
+
+            var binData=new byte[fajlnevHosszBin.Length+fajlhosszBin.Length+fajlnevBin.Length+szovegfajl.Length];
+
+            Console.WriteLine(binData.Length);
+
+            using (MemoryStream ms=new MemoryStream(binData))
+            {
+                using (BinaryWriter bw=new BinaryWriter(ms))
+                {
+                    bw.Write(fajlnevHosszBin);
+                    bw.Write(fajlnevBin);
+                    bw.Write(fajlhosszBin);
+                    bw.Write(szovegfajl);
+
+                }
+                File.WriteAllBytes("szinek.bin",ms.ToArray());
+                Console.WriteLine("Adatok fájlba írva");
+            }
+        }
+    }
+}
