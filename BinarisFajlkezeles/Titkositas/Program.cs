0a185f92faba993ab6d7a1b8a225260afd1d357e
kovacsrud <kovacs.rud@gmail.com>
2025-02-03 11:09:37 +0100

    Titkositas

---
+++ BinarisFajlkezeles/Titkositas/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+namespace Titkositas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Titkosítás");
+
+            string kulcs = "Titok_12";
+            string titkositando = "Szigorúan titkos tartalom";
+
+            Aes aes=Aes.Create();
+            SHA256 sha256 = SHA256.Create();
+
+            byte[] binKulcs = sha256.ComputeHash(Encoding.UTF8.GetBytes(kulcs));
+            byte[] titkositandoBin = Encoding.UTF8.GetBytes(titkositando);
+            aes.Key = binKulcs;
+            aes.GenerateIV();
+            
+            
+
+            ICryptoTransform titkosito = aes.CreateEncryptor(binKulcs,aes.IV);
+            byte[] titkositott = titkosito.TransformFinalBlock(titkositandoBin, 0, titkositandoBin.Length);
+
+            string kodoltSzoveg = Encoding.UTF8.GetString(titkositott);
+
+            Console.WriteLine(kodoltSzoveg);
+
+        }
+    }
+}
