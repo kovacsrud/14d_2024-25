9b063a2198c3c48f992b3565ccfa3c2006258a4e
kovacsrud <kovacs.rud@gmail.com>
2025-03-03 10:06:44 +0100

    Teszt riportolás

--- WpfHomersekletTest/HomersekletWPFTest/UnitTest1.cs
+++ WpfHomersekletTest/HomersekletWPFTest/UnitTest1.cs
@@ -3,6 +3,8 @@ using OpenQA.Selenium.Appium;
 using OpenQA.Selenium.Appium.Windows;
 using NUnit.Framework.Interfaces;
 using System;
+using AventStack.ExtentReports;
+using AventStack.ExtentReports.Reporter;
 
 namespace HomersekletWPFTest
 {
@@ -13,6 +15,22 @@ namespace HomersekletWPFTest
         private const string ProgramPath = @"D:\rud-2\kodtarak_2024\14d_2024-25\WpfHomersekletTest\WpfHomersekletTest\bin\Debug\net8.0-windows\";
 
         protected static WindowsDriver<WindowsElement> driver;
+
+        protected static ExtentReports extReport;
+        protected static ExtentTest extTest;
+
+        [OneTimeSetUp]
+        public static void ReportSetup()
+        {
+            extReport = new ExtentReports();
+            extReport.AddSystemInfo("Hõmérséklet átváltás teszt","Automatizált teszt");
+            extReport.AddSystemInfo("Tesztelõ","Teszt Ágnes");
+            ExtentSparkReporter reporter = new ExtentSparkReporter(ProgramPath+"\\result.html");
+            extReport.AttachReporter(reporter);
+            reporter.Config.DocumentTitle = "Hõmérséklet konvertálás teszt riport";
+            reporter.Config.ReportName = "Hõmérséklet konvertálás";
+            reporter.Config.Theme = AventStack.ExtentReports.Reporter.Config.Theme.Standard;
+        }
 
 
         [OneTimeSetUp]
@@ -33,9 +51,10 @@ namespace HomersekletWPFTest
         [TestCase(33,0.55555555)]
         [TestCase(21,-6.1111)]
         [TestCase(45, 7.2222)]
-        [TestCase(85, 29.444)]
+        [TestCase(85, 26.444)]
         public void FahrenheitToCelsiusTest(double fahrenheit,double elvart)
         {
+            extTest = extReport.CreateTest("Fahrenheit átváltása celsiusra");
             var homerseklet = driver.FindElementByAccessibilityId("textboxHomerseklet");
             homerseklet.Clear();
             driver.FindElementByAccessibilityId("celsiusKivalaszt").Click();
@@ -44,6 +63,7 @@ namespace HomersekletWPFTest
             var eredmeny = driver.FindElementByAccessibilityId("konvertaltHomerseklet");
             
             Assert.AreEqual(elvart,Convert.ToDouble(eredmeny.Text),0.001);
+            extTest.Log(Status.Pass,"Fahrenheit átváltása Celsiusra OK");
         }
 
         [Test]
@@ -52,6 +72,7 @@ namespace HomersekletWPFTest
         [TestCase(32,89.6)]
         public void CelsiusToFahrenheitTest(double celsius,double elvart)
         {
+            extTest = extReport.CreateTest("Celsius átváltása Fahrenheitre");
             var homerseklet = driver.FindElementByAccessibilityId("textboxHomerseklet");
             homerseklet.Clear();
             driver.FindElementByAccessibilityId("fahrenheitKivalaszt").Click();
@@ -60,7 +81,35 @@ namespace HomersekletWPFTest
             var eredmeny = driver.FindElementByAccessibilityId("konvertaltHomerseklet");
 
             Assert.AreEqual(elvart, Convert.ToDouble(eredmeny.Text), 0.001);
+            extTest.Log(Status.Pass, "Celsius átváltása fahrenheitre OK");
 
+        }
+
+        [TearDown]
+        public static void CloseReport()
+        {
+            var status = TestContext.CurrentContext.Result.Outcome.Status;
+            var stacktrace = TestContext.CurrentContext.Result.StackTrace;
+            var errormsg = TestContext.CurrentContext.Result.Message;
+
+            if (status==TestStatus.Failed)
+            {
+                ITakesScreenshot shot = (ITakesScreenshot)driver;
+                var be=TestContext.CurrentContext.Test.Arguments[0];
+                var elvart=TestContext.CurrentContext.Test.Arguments[1];
+                var filename = $"result_{be}_{elvart}.png";
+                Screenshot screenshot = shot.GetScreenshot();
+                screenshot.SaveAsFile(ProgramPath+filename,ScreenshotImageFormat.Png);
+                extTest.Log(Status.Fail,stacktrace+" "+errormsg);
+                extTest.Log(Status.Fail, "Képernyõ");
+                extTest.AddScreenCaptureFromPath(filename);
+
+
+            }
+
+
+
+            extReport.Flush();
         }
 
         [OneTimeTearDown]
